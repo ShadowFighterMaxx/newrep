d5589fbf3009e69a8d7695d018b1a32639394b69
Maxx <1annavarapu.sridatta@gmail.com>
2024-10-22 19:26:13 +0530

    Add files via upload

---
+++ pg.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iterations = 0
+    print("the number entered by you is:", n)
+    iterations+= 1
+    print("total iterations done are:", iterations)
+
+printnumber(10)
+printnumber(20)
+
