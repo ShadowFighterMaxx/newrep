ab3c5208285543c39ec5683f649345cf8a81fa70
Maxx <1annavarapu.sridatta@gmail.com>
2024-10-22 19:36:04 +0530

    Add files via upload

---
+++ dbs.py
@@ -0,0 +1,10 @@
+def onTime(n):
+    iteration = 0 
+    for c in range(1, n+1):
+        iteration+=1
+    print("No of iterations is:", iteration)
+n= int(input("Enter A Number"))
+onTime(n) 
+
+
+
